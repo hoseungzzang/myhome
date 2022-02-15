519628731c2e66d2f72c5b025ca072c2ed5b6e35
hoseungzzang <wnghtmd99@naver.com>
2022-02-15 22:05:31 +0900

    layout 구성

--- src/main/java/com/godcoder/myhome/controller/BoardController.java
+++ src/main/java/com/godcoder/myhome/controller/BoardController.java
@@ -2,12 +2,14 @@ package com.godcoder.myhome.controller;
 
 import com.godcoder.myhome.model.Board;
 import com.godcoder.myhome.repository.BoardRepository;
+import com.godcoder.myhome.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -16,10 +18,33 @@ public class BoardController {
 
     @Autowired
     private BoardRepository boardRepository;
-    @GetMapping("/list") //중요하진않음 결국 return부분에서 html문서의 이름을 가져다 써야함.
+    @Autowired //스프링 기동될때 인스턴스가 담김
+    private BoardValidator boardValidator;
+    @GetMapping("/list") //도메인에 표시되는 경로/ 중요하진않음 결국 return부분에서 html문서의 이름을 가져다 써야함.
     public String list(Model model){
         List<Board> boards = boardRepository.findAll();
         model.addAttribute("boards",boards);
         return "board/list";
+    }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id){
+        if(id ==null){
+            model.addAttribute("board", new Board());
+        }else{
+            Board board = boardRepository.findById(id).orElse(null);//수정 시 id에 해당하는 정보를 담음
+            model.addAttribute("board" ,board);
+        }
+
+        return "board/form";
+    }
+    @PostMapping("/form")
+    public String greetingSubmit(@Valid Board board, BindingResult bindingResult) {
+        boardValidator.validate(board,bindingResult);
+        if(bindingResult.hasErrors()){
+            return "board/form";
+        }
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
--- src/main/java/com/godcoder/myhome/model/Board.java
+++ src/main/java/com/godcoder/myhome/model/Board.java
@@ -2,10 +2,14 @@ package com.godcoder.myhome.model;
 
 import lombok.Data;
 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 //어노테이션 파트
 @Entity//JPA 설정을 위해서 모델클래스임을 알림
 @Data
@@ -13,6 +17,8 @@ public class Board {
     @Id//PK를 알려줌
     @GeneratedValue(strategy = GenerationType.IDENTITY)//mariaDB상에 pk가 자동증가로 설정되어있어 설정해줌
     private Long id;
+    @NotNull
+    @Size(min=2, max=30, message = "제목은 2자이상 30자 이하 입니다.")
     private String title;
     private String content;
 
